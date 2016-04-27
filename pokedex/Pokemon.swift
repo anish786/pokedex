81e28c0b17007c064e50bae19842be736991fd3e
Anish Ali <ali.anish91@yahoo.com>
2016-04-26 21:05:50 -0500

    added pokemons, labels, music, filtering

---
+++ pokedex/Pokemon.swift
@@ -0,0 +1,27 @@
+//
+//  Pokemon.swift
+//  pokedex
+//
+//  Created by ali.anish91 on 4/26/16.
+//  Copyright © 2016 SAAS. All rights reserved.
+//
+
+import Foundation
+
+class Pokemon {
+    private var _name: String!
+    private var _pokedexId: Int!
+    
+    var name: String {
+        return _name
+    }
+    
+    var pokedexId: Int {
+        return _pokedexId
+    }
+    
+    init(name: String, pokedexId: Int){
+        self._name = name
+        self._pokedexId = pokedexId
+    }
+}
